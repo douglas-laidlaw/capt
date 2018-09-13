15623429d082f5ff75a2ac7a2799d91c8c8e7483
Douglas Laidlaw <douglas.john.laidlaw@gmail.com>
2018-09-13 11:35:18 +0100

    test push

--- tests/test_canary_fitting.py
+++ tests/test_canary_fitting.py
@@ -9,7 +9,7 @@ def get_turbulenceProfile_information(configuration, air_mass, tas, pix_arc,
     
     conf = capt.turbulence_profiler(configuration)
     input_matrix = capt.turbulence_profiler.make_covariance_matrix(conf, air_mass, tas, conf.layer_alt, 
-        numpy.array([0.5,0.5]), conf.guess_L0, conf.tt_track, None, conf.shwfs_shift, conf.shwfs_rot)
+        numpy.array([0.8,0.8]), conf.guess_L0, conf.tt_track, None, conf.shwfs_shift, conf.shwfs_rot)
 
     conf = capt.turbulence_profiler(configuration)
     results = conf.perform_turbulence_profiling(air_mass, tas, pix_arc, shwfs_centroids, cov_matrix=input_matrix)
