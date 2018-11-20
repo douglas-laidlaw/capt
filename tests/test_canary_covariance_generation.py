4a642c6a355d0d7180990d84781b94a824146e35
Douglas Laidlaw <douglas.john.laidlaw@gmail.com>
2018-11-20 13:20:11 +0000

    test_canary_covariance_generation.py updated

--- tests/test_canary_covariance_generation.py
+++ tests/test_canary_covariance_generation.py
@@ -2,6 +2,7 @@ import capt
 import numpy
 from astropy.io import fits
 from matplotlib import pyplot; pyplot.ion()
+import capt.misc_functions.matplotlib_format
 
 
 def generate_covariance(configuration, air_mass, tas, pix_arc, 
@@ -10,20 +11,20 @@ def generate_covariance(configuration, air_mass, tas, pix_arc,
     conf = capt.turbulence_profiler(configuration)
     
     mat = capt.turbulence_profiler.make_covariance_matrix(
-        conf, air_mass, tas, conf.layer_alt, conf.guess_r0, conf.guess_L0, conf.tt_track, 
+        conf, conf.pupil_mask, air_mass, tas, conf.layer_alt, conf.guess_r0, conf.guess_L0, conf.tt_track, 
         False, conf.shwfs_shift, conf.shwfs_rot, l3s1_transform=False, target_array='Covariance Matrix', 
-        tt_track_present=False, offset_present=False, fitting_L0=True)
+        tt_track_present=False, offset_present=False)
 
-    # roi = capt.turbulence_profiler.make_covariance_roi(
-    #     conf, air_mass, tas, conf.layer_alt, conf.guess_r0, conf.guess_L0, conf.tt_track, 
-    #     False, conf.shwfs_shift, conf.shwfs_rot, l3s1_transform=False, tt_track_present=True, 
-    #     offset_present=True, fitting_L0=True)
+    roi = capt.turbulence_profiler.make_covariance_roi(
+        conf, conf.pupil_mask, air_mass, tas, conf.layer_alt, conf.guess_r0, conf.guess_L0, conf.tt_track, 
+        False, conf.shwfs_shift, conf.shwfs_rot, l3s1_transform=False, 
+        tt_track_present=True, offset_present=False)
 
     pyplot.figure()
     pyplot.imshow(mat)
-    # pyplot.figure()
-    # pyplot.imshow(roi)
-    return mat#, roi
+    pyplot.figure()
+    pyplot.imshow(roi)
+    return mat, roi
 
 if __name__ == '__main__':
 
@@ -33,5 +34,5 @@ if __name__ == '__main__':
     canary_tas = numpy.array([(0, 0), (0, 40.)])
     configuration = capt.configuration('../conf/canary_example_conf.yaml')
 
-    m = generate_covariance(
+    m, r = generate_covariance(
         configuration, air_mass, canary_tas, pix_arc, shwfs_centroids)
